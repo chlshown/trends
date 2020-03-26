18981dce58c1e73fe7b431b6770b354b30d54826
ray.chen <chlshown@163.com>
2020-03-26 16:28:32 +0800

    no message

---
+++ config.js
@@ -0,0 +1,87 @@
+const source = [{
+    index: 1,
+    sourceTabelName: 'AdhocAcquisitionsList',
+    recordTableName: 'AdhocAcquisitionsList_record'
+},{
+    index: 2,
+    sourceTabelName: 'ChineseMovies_NonScriptedAsian',
+    recordTableName: 'ChineseMovies_NonScriptedAsian_record1'
+},{
+    index: 3,
+    sourceTabelName: 'ChineseMovies_NonScriptedAsian',
+    recordTableName: 'ChineseMovies_NonScriptedAsian_record2'
+},{
+    index: 4,
+    sourceTabelName: 'ChineseSeriesTest',
+    recordTableName: 'ChineseSeriesTest_record1'
+},{
+    index: 5,
+    sourceTabelName: 'ChineseSeriesTest',
+    recordTableName: 'ChineseSeriesTest_record2'
+},{
+    index: 6,
+    sourceTabelName: 'JapAnimeSeries',
+    recordTableName: 'JapAnimeSeries_record1'
+},{
+    index: 7,
+    sourceTabelName: 'JapAnimeSeries',
+    recordTableName: 'JapAnimeSeries_record2'
+},{
+    index: 8,
+    sourceTabelName: 'KoreanMovies',
+    recordTableName: 'KoreanMovies_record1'
+},{
+    index: 9,
+    sourceTabelName: 'KoreanMovies',
+    recordTableName: 'KoreanMovies_record2'
+},{
+    index: 10,
+    sourceTabelName: 'KoreanSeries',
+    recordTableName: 'KoreanSeries_record1'
+},{
+    index: 11,
+    sourceTabelName: 'KoreanSeries',
+    recordTableName: 'KoreanSeries_record2'
+},{
+    index: 12,
+    sourceTabelName: 'Thai_JapMovies',
+    recordTableName: 'Thai_JapMovies_record1'
+},{
+    index: 13,
+    sourceTabelName: 'Thai_JapMovies',
+    recordTableName: 'Thai_JapMovies_record2'
+},{
+    index: 14,
+    sourceTabelName: 'Thai_JapSeries',
+    recordTableName: 'Thai_JapSeries_record1'
+},{
+    index: 15,
+    sourceTabelName: 'Thai_JapSeries',
+    recordTableName: 'Thai_JapSeries_record2'
+},{
+    index: 16,
+    sourceTabelName: 'Western_movies_batch_1',
+    recordTableName: 'Western_movies_batch_1_record1'
+},{
+    index: 17,
+    sourceTabelName: 'Western_movies_batch_1',
+    recordTableName: 'Western_movies_batch_1_record2'
+},{
+    index: 18,
+    sourceTabelName: 'Western_movies_batch_2',
+    recordTableName: 'Western_movies_batch_2_record1'
+},{
+    index: 19,
+    sourceTabelName: 'Western_movies_batch_2',
+    recordTableName: 'Western_movies_batch_2_record2'
+},{
+    index: 20,
+    sourceTabelName: 'Western_series',
+    recordTableName: 'Western_series_record1'
+},{
+    index: 21,
+    sourceTabelName: 'Western_series',
+    recordTableName: 'Western_series_record2'
+}]
+
+module.exports = {source}
